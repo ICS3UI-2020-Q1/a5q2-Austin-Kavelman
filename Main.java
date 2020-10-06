12033171ead1cc20c0bf4d859fbc4350b92fba76
AustinKavelman <kavea9410@wrdsb.ca>
2020-10-06 13:59:54 +0000

    finished positive number loop.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * this program enters 1 to the number the user enters
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //creates variable for the number entered
+    int n, i;
     
+    //asks the user to enter a positive interger
+    System.out.println("Please enter a positive integer");
+    n = input.nextInt();
+
+    //for loop that counts from 1 until it reaches the number entered
+    for(i = 1; i >= 0; i++){
+      System.out.print(i + ",");
+    //when the entered number is reached stop the loop
+    if(n==i)
+      break;
+    }
+
   }
 }
